e192b5278ecee5e3d153c218c335e796c95a5f05
DestroyerV <mishravaibhav12321@gmail.com>
2024-10-24 22:52:28 +0530

    feat: Add ThemeProvider component and update layout and page components
    - Add ThemeProvider component to provide theme functionality
    - Update layout component to wrap children with ThemeProvider
    - Update page component to import new components and use HeroHighlight component
    - Update tailwind.config.ts to add new utility classes for background color patterns
    - Update package.json and pnpm-lock.yaml to add new dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,11 +25,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider attribute='class' defaultTheme='dark'>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['images.unsplash.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
